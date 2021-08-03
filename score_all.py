b71cbd9a6e811b1a6f753bbf675779046da9355e
11973 <1197386970@qq.com>
2021-08-03 11:34:53 +0800

    initial

--- score_all.py
+++ score_all.py
@@ -13,6 +13,7 @@ for k in range(6):
         try:
             # df_temp =  pd.read_excel('./score_all/1.xls',index_col= 0,sheet_name=k)
             df_temp = pd.read_excel(filename, index_col=0, sheet_name=l)
+            # df_temp = pd.read_excel(filename, index_col=None, sheet_name=l)
             # print('yes')
         except:
             message = filename +'sheet'+str(l)+'读取失败'
@@ -30,6 +31,7 @@ print(df1)
 df2 = df1.sort_values("智育成绩",inplace=False,ascending= False)
 #这里的inplace是对下面的操作进行选择，False原Dataframe不变，如果True原Dataframe发生改变
 #这里的ascending是对升序降序的选择
+#第一个是by参数，按照什么列进行排序
 
 
 df1.to_excel('output.xlsx',sheet_name='全部数据')
@@ -44,3 +46,12 @@ print(df3)
 # df2 = pd.read_excel('./score_all/1.xls',index_col= 0,sheet_name=1)
 # print(df1)
 # print(df1[:,2])
+#直接给出数据的一些统计特征
+#均值 mean 标准差std 最小值min 百分位数 max最大值
+print(df3.describe())
+print(df.loc[df['智育成绩'] > 90])#这里做的一个数据的筛选
+print(df.iloc[[0,1],:]) #按行号列号进行求解
+print(df.iloc[:,[0,1]]) #按行号列号进行求解
+#查看数据缺失情况，做出对应的调整
+# print(df3.info())
+# df3.dropna(axis=0,how='all')
