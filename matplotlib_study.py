7ca1d841052d4f375dbe970df872b03ea00d7ea7
11973 <1197386970@qq.com>
2021-08-05 21:11:04 +0800

    20210805

--- matplotlib_study.py
+++ matplotlib_study.py
@@ -55,3 +55,33 @@ plt.show()
 # plt.setp(lines, 'color', 'r', 'linewidth', 2.0)
 # plt.show()
 
+#下面开始绘制直方图
+mu, sigma = 100, 15
+x = mu + sigma * np.random.randn(10000)
+# the histogram of the data
+n, bins, patches = plt.hist(x, 50, density=1, facecolor='g', alpha=0.75)
+plt.xlabel('Smarts')
+plt.ylabel('Probability')
+plt.title('Histogram of IQ')
+plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+plt.axis([40, 160, 0, 0.03])
+plt.grid(True)
+plt.show()
+
+#绘制散点图
+plt.figure(figsize=(20,20),dpi= 60)
+x = np.array([1,2,3,4])
+y = np.array([1,2,3,4])
+plt.scatter(x,y)
+plt.show()
+#bar:条形图
+film_name = ['战狼2','绣春刀','机器侠','美人鱼','哆啦A梦']
+film_score = [100,90,100,80,70]
+plt.bar(film_name,film_score,color = 'r',width= 0.5)
+plt.show()
+#barh:横向的条形图
+film_name = ['战狼2','绣春刀','机器侠','美人鱼','哆啦A梦']
+film_score = [100,90,100,80,70]
+plt.barh(film_name,film_score,color = 'orange',height= 0.5)
+plt.show()
+
